41eb063263a8e4593a3bc209ebb9a8a895fa4b43
JoshuaJoelMunoz <joshuajoelmunoz@knights.ucf.edu>
2017-03-01 23:51:46 -0500

    Fixing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -217,30 +217,3 @@
 	});
 
 })(jQuery);
-
-$(document).ready(function() {
-
-	$(".fancybox").fancybox({
-		maxWidth	: 1280,
-		maxHeight	: 720,
-		fitToView	: false,
-		width		: '80%',
-		height		: '80%',
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'elastic',
-		closeEffect	: 'elastic',
-		padding : 0
-	});
-
-	$(".image").fancybox({
-		openEffect	: 'elastic',
-		closeEffect	: 'elastic',
-
-		helpers : {
-			title : {
-				type : 'over'
-			}
-		}
-	});
-});
