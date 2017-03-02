01cc1272388bd94abeef61bd0019e2b8a097a811
JoshuaJoelMunoz <joshuajoelmunoz@knights.ucf.edu>
2017-03-01 22:22:31 -0500

    Test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -217,3 +217,30 @@
 	});
 
 })(jQuery);
+
+$(document).ready(function() {
+
+	$(".fancybox").fancybox({
+		maxWidth	: 1280,
+		maxHeight	: 720,
+		fitToView	: false,
+		width		: '80%',
+		height		: '80%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+		padding : 0
+	});
+
+	$(".image").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+
+		helpers : {
+			title : {
+				type : 'over'
+			}
+		}
+	});
+});
